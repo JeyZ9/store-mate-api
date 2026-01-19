b57f6dc99089fa863c0be8304b89b02ed8e9a38d
JeyZ9 <ggjj4511@gmail.com>
2026-01-19 19:47:39 +0700

    Start project

---
+++ src/test/java/com/sm/jeyz9/storemateapi/StoreMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.jeyz9.storemateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
