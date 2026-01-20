5e3c4c56380a698d4698535b2f883fd84e9fd975
JeyZ9 <ggjj4511@gmail.com>
2026-01-20 17:22:42 +0700

    set profile test

--- src/test/java/com/sm/jeyz9/storemateapi/StoreMateApiApplicationTests.java
+++ src/test/java/com/sm/jeyz9/storemateapi/StoreMateApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.sm.jeyz9.storemateapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StoreMateApiApplicationTests {
 
     @Test
