13c8e6901255111dcc441987c2939d6e2c8fb83d
FamSp <tame2543@gmail.com>
2026-03-04 11:00:39 +0700

    get update remove Cart item

--- src/main/java/com/sm/jeyz9/storemateapi/controllers/CartController.java
+++ src/main/java/com/sm/jeyz9/storemateapi/controllers/CartController.java
@@ -1,17 +1,16 @@
 package com.sm.jeyz9.storemateapi.controllers;
 
 import com.sm.jeyz9.storemateapi.dto.CartItemRequestDTO;
+import com.sm.jeyz9.storemateapi.models.CartItem;
 import com.sm.jeyz9.storemateapi.services.CartService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,4 +26,26 @@ public class CartController {
     public ResponseEntity<String> addProductToCart(@Valid @RequestBody CartItemRequestDTO request, Principal principal) {
            return new ResponseEntity<>(cartService.addProductToCart(principal.getName(), request), HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getCart(Principal principal) {
+        return ResponseEntity.ok(cartService.getCartItems(principal.getName()));
+    }
+
+    @PatchMapping("/items/{productId}/increment")
+    public ResponseEntity<String> increment(@PathVariable Long productId, Principal principal) {
+        return ResponseEntity.ok(cartService.updateQuantity(principal.getName(), productId, 1));
+    }
+
+    @PatchMapping("/items/{productId}/decrement")
+    public ResponseEntity<String> decrement(@PathVariable Long productId, Principal principal) {
+        return ResponseEntity.ok(cartService.updateQuantity(principal.getName(), productId, -1));
+    }
+
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<String> remove(@PathVariable Long productId, Principal principal) {
+        return ResponseEntity.ok(cartService.removeItem(principal.getName(), productId));
+    }
+
+
 }
