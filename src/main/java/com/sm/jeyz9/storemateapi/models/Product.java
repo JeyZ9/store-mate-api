bedcc49121e7cf9d04c417ccbd0898374ef27678
JeyZ9 <ggjj4511@gmail.com>
2026-03-12 13:28:36 +0700

    remove summery from product

--- src/main/java/com/sm/jeyz9/storemateapi/models/Product.java
+++ src/main/java/com/sm/jeyz9/storemateapi/models/Product.java
@@ -50,7 +50,6 @@ public class Product {
     private List<ProductImage> productImage;
     
     private Integer stock_quantity;
-    private String summary;
     
     @Column(columnDefinition = "TEXT")
     private String description;
